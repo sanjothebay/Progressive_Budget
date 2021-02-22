511d9bb213ae66fd6cefa0388a497bbf1e5142c5
JC <sanjothebay@gmail.com>
2021-02-21 22:41:36 -0600

    added to my indexedDB on top

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -1,6 +1,14 @@
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+
 let db;
 // create a new db request for a "budget" database.
-const request = indexedDB.open("budget", 1);
+const request = indexedDB.open("transaction", 1);
 
 request.onupgradeneeded = function(event) {
    // create object store called "pending" and set autoIncrement to true
